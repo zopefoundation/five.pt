96077e273b02eb8f621d1423128d86d704c9fa0e
Hanno Schlichting <hanno@hannosch.eu>
2008-08-12 22:24:28 +0000

    More tests to prove all our local variables are actually available

--- src/five/pt/tests/test_pagetemplatefile.py
+++ src/five/pt/tests/test_pagetemplatefile.py
@@ -11,6 +11,22 @@ class SimpleView(BrowserView):
     index = ViewPageTemplateFile('simple.pt')
 
 
+class LocalsView(BrowserView):
+
+    def available(self):
+        return 'yes'
+
+    def tagsoup(self):
+        return '<foo></bar>'
+
+    index = ViewPageTemplateFile('locals.pt')
+
+
+class OptionsView(BrowserView):
+
+    index = ViewPageTemplateFile('options.pt')
+
+
 class TestPageTemplateFile(ZopeTestCase):
 
     def afterSetUp(self):
@@ -20,13 +36,33 @@ class TestPageTemplateFile(ZopeTestCase):
         zcml.load_config("configure.zcml", Products.Five)
         zcml.load_config("configure.zcml", z3c.pt)
 
-    def test_simplefile(self):
-        context = self.folder
-        request = context.REQUEST
-
-        view = SimpleView(context, request)
+    def test_simple(self):
+        view = SimpleView(self.folder, self.folder.REQUEST)
         result = view.index()
         self.failUnless('Hello World' in result)
+
+    def test_locals(self):
+        view = LocalsView(self.folder, self.folder.REQUEST)
+        result = view.index()
+        self.failUnless("view:yes" in result)
+        self.failUnless('Folder at test_folder_1_' in result)
+        self.failUnless('http://nohost' in result)
+        self.failUnless('here==context:True' in result)
+        self.failUnless('here==container:True' in result)
+        self.failUnless("root:(\'\',)" in result)
+        self.failUnless("nothing:None" in result)
+        self.failUnless("modules:&amp;lt;foo&amp;gt;" in result)
+
+    def test_options(self):
+        view = OptionsView(self.folder, self.folder.REQUEST)
+        options = dict(
+            a=1,
+            b=2,
+            c='abc',
+        )
+        result = view.index(**options)
+        self.failUnless("a : 1" in result)
+        self.failUnless("c : abc" in result)
 
 
 def test_suite():
