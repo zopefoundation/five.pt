6684585107e93a04f9cd84b5b812252a21e9eb91
Malthe Borch <mborch@gmail.com>
2008-11-16 22:43:39 +0000

    Added monkey-patches for module-level view page template instances.

---
+++ src/five/pt/patches.py
@@ -0,0 +1,29 @@
+"""Monkey-patching page template classes.
+
+Since many templates are instantiated at module-import, we patch using
+a duck-typing strategy.
+
+We replace the ``__get__``-method of the ViewPageTemplateFile class
+(both the Five variant and the base class). This allows us to return a
+Chameleon template instance, transparent to the calling class.
+"""
+
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile as \
+     ZopeViewPageTemplateFile
+
+from Products.Five.browser.pagetemplatefile import BoundPageTemplate
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile as \
+     FiveViewPageTemplateFile
+     
+from five.pt.pagetemplate import ViewPageTemplateFile
+
+_marker = object()
+
+def get_bound_template(self, instance, type):
+    template = getattr(self, '_template', _marker)
+    if template is _marker:
+        self._template = template = ViewPageTemplateFile(self.filename)
+    return BoundPageTemplate(template, instance)
+
+FiveViewPageTemplateFile.__get__ = get_bound_template
+ZopeViewPageTemplateFile.__get__ = get_bound_template
