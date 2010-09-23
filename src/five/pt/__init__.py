5943438cb4c78dcf51908f3fecd2773ac150bc92
Malthe Borch <mborch@gmail.com>
2010-09-23 07:20:43 +0000

    Support eager loading of patched templates (via gc lookup).

--- src/five/pt/__init__.py
+++ src/five/pt/__init__.py
@@ -1,1 +1,18 @@
-#
+def initialize(site):
+    import patches
+    import chameleon.core.config
+
+    if chameleon.core.config.EAGER_PARSING:
+        import gc
+
+        # this only deals with the page template classes provided by
+        # Zope, as Chameleon's classes are aware of the eager parsing
+        # flag
+        from zope.pagetemplate.pagetemplatefile import PageTemplateFile
+        from five.pt.pagetemplate import BaseTemplateFile
+
+        for template in (x for x in gc.get_objects()
+                         if isinstance(x, PageTemplateFile)):
+
+            inst = BaseTemplateFile(template.filename)
+            inst.parse()
